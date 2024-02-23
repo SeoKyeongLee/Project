662a5364cfee4071d66b5e97462495c585018597
SeoKyeong Lee <153425876+SeoKyeongLee@users.noreply.github.com>
2024-02-23 15:39:05 +0900

    Delete src/goldenTime directory

--- src/goldenTime/command/BCommand.java
+++
@@ -1,9 +0,0 @@
-package goldenTime.command;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface BCommand {
-	void execute(HttpServletRequest request, HttpServletResponse response);
-	
-}
